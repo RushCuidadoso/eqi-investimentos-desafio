cb1a206bcaf622d2a6100fbeb4051e6820b5c773
RushCuidadoso <d3nisfp@gmail.com>
2022-02-14 15:28:58 -0400

    fim dia 14/02/22

--- script.js
+++ script.js
@@ -3,8 +3,36 @@ simular[0].addEventListener('click', ()=>{
     fetch("http://localhost:3000/simulacoes")
     .then(response => response.json())
     .then(response => console.log(response))
-
-    teste = document.getElementsByName('cdi')
-    console.log(teste[0].value)
 })
 
+limpar = document.getElementsByClassName("limpar")
+limpar[0].addEventListener('click', ()=>{
+    texto = document.querySelectorAll("input[type=text]")
+    for (let i = 0; i < texto.length; i++) {
+        texto[i].value = ""; 
+    }
+    numero = document.querySelectorAll("input[type=number]")
+    for (let i = 0; i < numero.length; i++) {
+        numero[i].value = ""; 
+    }
+})
+
+bruto = document.getElementsByClassName("bruto")
+liquido = document.getElementsByClassName("liquido")
+pre = document.getElementsByClassName("pre")
+pos = document.getElementsByClassName("pos")
+fixado = document.getElementsByClassName("fixado")
+
+bruto[0].addEventListener('click', ()=>{
+    bruto[0].classList.add("active")
+    liquido[0].classList.remove("active")
+})
+liquido[0].addEventListener('click', ()=>{
+    bruto[0].classList.remove("active")
+    liquido[0].classList.add("active")
+})
+
+pre[0].addEventListener('click', ()=>{
+    pre[0].classList.add("active")
+    liquido[0].classList.add("active")
+})
